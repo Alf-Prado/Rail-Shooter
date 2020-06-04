60214c4a379f6deec3e6dbdaacaf6905c74744d0
Alonso <a01650552@itesm.mx>
2020-06-04 17:41:10 -0500

    fsm changes

---
+++ Rail Shooter V2/Assets/Scripts/FSM/FSM2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM2 : MonoBehaviour
+{
+    public Texture2D texture;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        //texture = new Texture2D(512, 512, TextureFormat.RGBAFloat, false);
+        texture = new Texture2D(512, 512, TextureFormat.RGBAHalf, false);
+        GetComponent<Renderer>().material.mainTexture = texture;
+        texture.Apply();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
